e025fc56449003b6006e51ae18d44d27e8dce71f
Svarog <feanorr@gmail.com>
2015-03-14 15:01:26 +0200

    Initial fractal

--- demo.js
+++ demo.js
@@ -1,4 +1,8 @@
 var m = Math;
+var base = new C(m.E, 0);
+var canvas = document.getElementsByTagName("canvas")[0];
+var size = 50;
+var drawn = {};
 
 function C(r, i){
   return {
@@ -28,4 +32,41 @@ function C(r, i){
       return (t < 0) ? m.PI + t : t;
     }
  }
+}
+
+canvas.addEventListener('mousemove', function(e) {
+  // get mouse position
+  var rect = canvas.getBoundingClientRect();
+  for(var i = -25; i < 25; i++){
+    for(var j = -25; j < 25; j++){
+      draw(e.clientX - rect.left + i, e.clientY - rect.top + j);
+    }
+  }
+});
+
+
+function itCounter(x){
+  for (var i = 0; i < 20; i++){
+    x = base.e(x);
+    if(x.abs() > 50){
+      return [i, x.t()];
+    }
+  }
+  return [20, 0];
+}
+
+function draw(x, y){
+  if(!drawn[x+","+y]){
+    console.log("start");
+    drawn[x+","+y] = true;
+    var val = new C(4*(x / canvas.offsetWidth) - 2, 
+                    2*(y / canvas.offsetHeight) - 1);
+    var ctx = canvas.getContext("2d");
+    var itCounterResult = itCounter(val);
+    var it = m.floor((255/20)*(20 - itCounterResult[0]));
+    var phase = m.floor((256*((itCounterResult[1])/(2*m.PI))));
+    ctx.fillStyle = "rgb(" + (it+2*phase)/3 + "," + (it+phase)/2 +", " + (2*it+phase)/3 + ")";
+    ctx.fillRect(x, y, 1, 1);
+    console.log("end");
+  }
 }
